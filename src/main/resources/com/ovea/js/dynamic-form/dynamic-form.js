5a883866285c542f27cfe0ba312e71211f7d7085
mathieucarbou <mathieu.carbou@gmail.com>
2011-06-08 23:00:25 -0400

    improved dynamic form

--- src/main/resources/com/ovea/js/dynamic-form/dynamic-form.js
+++ src/main/resources/com/ovea/js/dynamic-form/dynamic-form.js
@@ -1,114 +1,90 @@
 if (window.DynamicForm == undefined) {
     (function($) {
 
-
         window.DynamicForm = function(form, options) {
-
-            self = this;
+            var self = this;
             this.form = form;
-            this.options = $.extend({
-                choicesLength: 4
-            }, options || {});
-
             this.template = {};
+            this.renderings = {};
+            this.options = $.extend({
+                    choicesLength: 4
+                }, options || {});
 
-            this.addTemplate('form', '<form id={id}></form>');
-            this.addTemplate('section', '<div id="{name}">');
-            this.addTemplate('question', '<div id="question_{name}"></div>');
-            this.addTemplate('question_label', '<p>{label}</p>');
-            this.addTemplate('radio', '<label>{value}<input type="radio" name="{name}" value="{value}"/></label>');
-            this.addTemplate('checkbox', '<label>{value}<input type="checkbox" name="{name}" value="{value}"/></label>');
-            this.addTemplate('text', '<label>{name}<input type="text" name="{name}"/></label>');
-            this.addTemplate('select', '<select name="{name}"></select>');
-            this.addTemplate('option', '<option value="{value}">{value}</option>');
-        };
+            this.addTemplate('form', '<form class="df-form" id="{id}"></form>');
+            this.addTemplate('sections', '<div class="df-sec" id="{name}">');
+            this.addTemplate('questions', '<div class="df-qu" id="question_{name}"></div>');
+            this.addTemplate('group', '<fieldset class="df-group" id="{name}"><legend>{label}</legend></fieldset>');
+            this.addTemplate('label', '<p class="df-lbl">{label}</p>');
 
-        function renderFieldsFor(element, question) {
-            var view = {};
-            view.name = question.name;
+            this.addTemplate('radio', '<label class="df-radio">{value}</label><input class="df-radio" type="radio" name="{name}" value="{value}"/>');
+            this.addTemplate('checkbox', '<label class="df-cb">{value}</label><input class="df-cb" type="checkbox" name="{name}" value="{value}"/>');
+            this.addTemplate('open', '<label class="df-open">{name}</label><input class="df-open" type="text" name="{name}"/>');
+            this.addTemplate('select', '<select class="df-sel" name="{name}"></select>');
+            this.addTemplate('option', '<option class="df-opt" value="{value}">{value}</option>');
 
-            // Render closed
-            if (question.type == 'closed') {
-                if (question.choices.length > self.options.choicesLength) {
-                    renderSelect(element, question);
-                } else {
-                    if (question.unique) {
-                        renderRadio(element, question);
-                    } else {
-                        renderCheckbox(element, question);
-                    }
+            this.addRendering('*', function (target, json, template) {
+                if (template) {
+                    var html = template.merge(json);
+                    target.append(html);
+                    return html;
                 }
-                return;
-            }
+            });
+            this.addRendering('questions', function (section, question, template) {
+                var html = template.merge(question);
+                if (question.type != 'group' && question.label && this.template['label']) {
+                    html.append(this.template['label'].merge(question));
+                }
+                this.renderBody(html, question, question.type);
+                section.append(html);
 
-            if (question.type == 'open') {
-
+            });
+            this.addRendering('closed', function (body, question) {
+                this.renderBody(body, question,
+                    question.choices.length > this.options.choicesLength ?
+                        'select' :
+                        (question.unique ? 'radio' : 'checkbox'));
+            });
+            this.addRendering('open', function (body, question, template) {
                 if (question.choices) {
-                    view.name = 'other';
-                    if (question.choices.length > self.options.choicesLength) {
-                        renderSelect(element, question);
-                    } else {
-                        if (question.unique) {
-                            renderRadio(element, question);
-                        } else {
-                            renderCheckbox(element, question);
-                        }
-                    }
-
+                    this.renderBody(body, question, 'closed');
+                    body.append(template.merge({
+                            name: question.name + '_other'
+                        }));
+                } else {
+                    body.append(template.merge(question));
                 }
-
-                element.append(self.template['text'].merge(view));
                 if (question.length) {
-                    element.find(':input').attr('maxlength', question.length);
+                    body.find(':input').attr('maxlength', question.length);
                 }
-            }
-        }
-
-        function renderRadio(element, question) {
-            var view = {};
-            view.name = question.name;
-            for each (var choice in question.choices) {
-                view.value = choice;
-                element.append(self.template['radio'].merge(view));
-            }
-
-            for each(var selected in question.selected) {
-                element.find('[value="' + selected + '"]').attr('checked', 'checked');
-            }
-        }
-
-        function renderCheckbox(element, question) {
-            var view = {};
-            view.name = question.name;
-            for each (var choice in question.choices) {
-                view.value = choice;
-                element.append(self.template['checkbox'].merge(view));
-            }
-
-            for each(var selected in question.selected) {
-                element.find('[value="' + selected + '"]').attr('checked', 'checked');
-            }
-        }
-
-        function renderSelect(element, question) {
-            var view = {};
-            view.name = question.name;
-
-            element.append(self.template['select'].merge(question));
-            if (!question.unique)
-                element.find('select').attr('multiple', 'true');
-
-            var select = element.find('select');
-            for each (var choice in question.choices) {
-                view.value = choice;
-                select.append(self.template['option'].merge(view));
-            }
-
-            for each(var selected in question.selected) {
-                select.find('option[value="' + selected + '"]').attr('selected', true);
-            }
-        }
-
+            });
+            this.addRendering(['checkbox', 'radio'], function (body, question, template) {
+                for (var c in question.choices) {
+                    body.append(template.merge({
+                            name: question.name,
+                            value: question.choices[c]
+                        }));
+                }
+                for (var s in question.selected) {
+                    body.find('[value="' + question.selected[s] + '"]').attr('checked', 'checked');
+                }
+            });
+            this.addRendering('select', function (body, question, template) {
+                body.append(template.merge(question));
+                var select = body.find('select');
+                if (!question.unique) {
+                    select.attr('multiple', 'true');
+                }
+                for (var c in question.choices) {
+                    select.append(this.template['option'].merge({
+                            name: question.name,
+                            value: question.choices[c]
+                        }));
+                }
+                for (var s in question.selected) {
+                    select.find('option[value="' + question.selected[s] + '"]').attr('selected', true);
+                }
+            });
+        };
 
         DynamicForm.prototype = {
 
@@ -119,7 +95,7 @@ if (window.DynamicForm == undefined) {
                 return this.form.sections;
             },
             section: function(name) {
-                for each (var section in this.form.sections) {
+                for (var section in this.form.sections) {
                     if (section.name == name)
                         return section;
                 }
@@ -127,55 +103,60 @@ if (window.DynamicForm == undefined) {
             },
             questions: function() {
                 var questions = [];
-                for each (var section in this.form.sections) {
-                    for each (var question in section.questions) {
+                for (var section in this.form.sections) {
+                    for (var question in section.questions) {
                         questions.push(question);
                     }
                 }
                 return questions;
             },
             question: function(name) {
-                for each (var question in this.questions()) {
+                for (var question in this.questions()) {
                     if (question.name == name)
                         return question;
                 }
                 return undefined;
             },
             render: function(target) {
-                var form = $('#' + target).append(this.template['form'].merge(this.form));
-                form.hide();
-
-                for each (var section in this.form.sections) {
-                    $('form', form).append(this.template['section'].merge(section));
-
-                    for each(var question in section.questions) {
-                        $('form', form).find('#' + section.name).append(this.template['question'].merge(question));
-                        var element = $('form', form).find('#question_' + question.name);
-                        if (question.label) {
-                            element.append(this.template['question_label'].merge(question));
-                        }
-
-                        renderFieldsFor(element, question);
+                var dynaform = $('<div class="df-container"/>');
+                var html = this.renderBody(dynaform, this.form, 'form');
+                $('#' + target).append(dynaform);
+            },
+            renderBody: function(target, json, type) {
+                if ($.isArray(json)) {
+                    for (var i in json) {
+                        this.renderBody(target, json[i], type);
+                    }
+                } else if (typeof json == 'object') {
+                    var func = this.renderings[type] || this.renderings['*'];
+                    var content = func.call(this, target, json, this.template[type]);
+                    for (var containerId in json) {
+                        this.renderBody(content || target, json[containerId], containerId);
                     }
                 }
-
-                form.show();
             },
-
             addTemplate: function(name, template) {
                 this.template[name] = {
                     merge: function(data) {
-                        return template.replace(/\{([\w\.]*)\}/g, function (str, key) {
+                        return $(template.replace(/\{([\w\.]*)\}/g, function (str, key) {
                             var keys = key.split("."), value = data[keys.shift()];
                             $.each(keys, function () {
                                 value = value[this];
                             });
                             return (value === null || value === undefined) ? "" : value;
-                        });
+                        }));
                     }
                 }
+            },
+            addRendering: function(name, func) {
+                if ($.isArray(name)) {
+                    for (var i in name) {
+                        this.renderings[name[i]] = func;
+                    }
+                } else {
+                    this.renderings[name] = func;
+                }
             }
-
         }
 
     })(jQuery);
--- src/test/javascript/data/survey_1.js
+++ src/test/javascript/data/survey_1.js
@@ -2,78 +2,98 @@ var survey_1 = {
     "id": "survey_1",
     "description": "employee_feedback",
     "sections":
-            [
-                {
-                    "name": "main_section",
-                    "questions": [
-                        {
-                            "name": "gender",
-                            "label": "specify_gender",
-                            "type": "closed",
-                            "unique": true,
-                            "choices": ["male","female"],
-                            "selected": ["female"]
+        [
+            {
+                "name": "main_section",
+                "questions": [
+                    {
+                        "name": "gender",
+                        "label": "specify_gender",
+                        "type": "closed",
+                        "unique": true,
+                        "choices": ["male","female"],
+                        "selected": ["female"]
 
-                        },
-                        {
-                            "name": "department",
-                            "label": "working_department",
-                            "type": "closed",
-                            "unique": false,
-                            "choices": ["administration", "finance", "marketing", "sales"],
-                            "selected": ["sales", "finance"]
+                    },
+                    {
+                        "name": "department",
+                        "label": "working_department",
+                        "type": "closed",
+                        "unique": false,
+                        "choices": ["administration", "finance", "marketing", "sales"],
+                        "selected": ["sales", "finance"]
 
-                        },
-                        {
-                            "name": "age",
-                            "type": "open",
-                            "selected": ["55"],
-                            "length": 3
-                        },
-                        {
-                            "name": "city",
-                            "label": "city",
-                            "type": "closed",
-                            "unique": true,
-                            "choices": ["quebec", "montreal", "laval", "levis", "brossard"],
-                            "selected": ["laval"]
-                        },
-                        {
-                            "name": "country",
-                            "label": "country",
-                            "type": "closed",
-                            "unique": false,
-                            "choices": ["usa", "canada", "spain", "germany", "mexico", "italy"],
-                            "selected": ["spain", "italy"]
-                        },
-                        {
-                            "name": "cookie",
-                            "label": "cookie",
-                            "type": "open",
-                            "unique": false,
-                            "length": 25,
-                            "choices": ["almond", "caramel", "chocolat"]
-                        },
-                        {
-                            "name": "car",
-                            "label": "car",
-                            "type": "open",
-                            "unique": true,
-                            "length": 25,
-                            "choices": ["honda", "nissan", "ford", "audi"]
-                        }
-                            ,
-                        {
-                            "name": "are_you_ok",
-                            "label": "are_you_ok",
-                            "type": "closed",
-                            "unique": false,
-                            "choices": ["Yes", "No"]
-                        }
-                    ]
-                },
-                {
-                    "name": "other_section"
-                }
-            ]
+                    },
+                    /*{
+                        "name": "groupe_one",
+                        "type": "group",
+                        "label": "age_and_city",
+                        "questions":
+                            [
+                                {
+                                    "name": "age",
+                                    "type": "open",
+                                    "length": 3
+                                },
+                                {
+                                    "name": "city",
+                                    "label": "city",
+                                    "type": "closed",
+                                    "unique": true,
+                                    "choices": ["quebec", "montreal", "laval", "levis", "brossard"],
+                                    "selected": ["laval"]
+                                }
+                            ]
+                    },*/
+                    {
+                        "name": "age",
+                        "type": "open",
+                        "length": 3
+                    },
+                    {
+                        "name": "city",
+                        "label": "city",
+                        "type": "closed",
+                        "unique": true,
+                        "choices": ["quebec", "montreal", "laval", "levis", "brossard"],
+                        "selected": ["laval"]
+                    },
+                    {
+                        "name": "country",
+                        "label": "country",
+                        "type": "closed",
+                        "unique": false,
+                        "choices": ["usa", "canada", "spain", "germany", "mexico", "italy"],
+                        "selected": ["spain", "italy"]
+                    },
+                    {
+                        "name": "cookie",
+                        "label": "cookie",
+                        "type": "open",
+                        "unique": false,
+                        "length": 25,
+                        "choices": ["almond", "caramel", "chocolat"]
+                    },
+                    {
+                        "name": "car",
+                        "label": "car",
+                        "type": "open",
+                        "unique": true,
+                        "length": 25,
+                        "choices": ["honda", "nissan", "ford", "audi"]
+                    }
+                    ,
+                    {
+                        "name": "are_you_ok",
+                        "label": "are_you_ok",
+                        "type": "closed",
+                        "unique": false,
+                        "choices": ["Yes", "No"]
+                    }
+                ]
+            },
+            {
+                "name": "other_section"
+            }
+        ]
 };
